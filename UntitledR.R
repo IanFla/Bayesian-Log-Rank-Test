6368381b8822a23865fab8cc27072e6f04ab3def
IanFla <51277149+IanFla@users.noreply.github.com>
2022-12-18 00:24:32 +0800

    hh

---
+++ UntitledR.R
@@ -0,0 +1,23 @@
+cal.norm <- function() {
+    pnorm(1) - pnorm(-1)
+}
+
+cal.t <- function(df) {
+    s <- sqrt(df / (df - 2))
+    
+    pt(s, df) - pt(-s, df)
+}
+
+cal.gamma <- function(a) {
+    m <- a
+    s <- sqrt(a)
+    
+    pgamma(m + s, a, 1) - pgamma(m - s, a, 1)
+}
+
+cal.beta <- function(a, b) {
+    m <- a / (a + b)
+    s <- sqrt(a * b / (a + b + 1)) / (a + b)
+    
+    pbeta(m + s, a, b) - pbeta(m - s, a, b)
+}
